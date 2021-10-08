de352befe82f6c42ec116e240676c6d70e12d1ed
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-10-07 19:10:27 -0600

    Server side

---
+++ 20211007/webapp1/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace webapp1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Filter { get; set; }
+        public string Message { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+              Message = "Resultados de " + Filter;
+        }
+    }
+}
